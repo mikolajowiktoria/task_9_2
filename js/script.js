0894da5343f5e753e4f380e5200de3f0e9e69599
Wiktoria Mikołajów <mikolajow.w@gmail.com>
2018-01-03 19:42:27 +0100

    Change javascript

--- js/script.js
+++ js/script.js
@@ -1,10 +1,9 @@
 var femaleNames = ['Asia', 'Kasia', 'Ola', 'Jola'];
-var femaleNames = ['Asia', 'Kasia', 'Ola', 'Jola'];
 var maleNames = ['Piotrek', 'Marek', 'Arek', 'Jarek'];
 var allNames = femaleNames.concat(maleNames);
 var newName = 'Marian';
 function pushNames() {
-  if (allNames.indexOf(newName) === -1);{
+  if (allNames.indexOf(newName) === -1) {
   return allNames.push(newName);
   }
 }
